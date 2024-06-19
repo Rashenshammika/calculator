884af93c83c6f79261f070d141411cc50e17a4d1
Rashenshammika <shenwotsan03@gmail.com>
2024-06-19 11:04:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+function calc() {
+
+
+let number01;
+let number02;
+
+number01 =new Number (document.getElementById("number01"));
+number02 =new Number (document.getElementById("number02"));
+
+let op=document.getElementById("op");
+let lblOutput = document.getElementById("output");
+
+console.log(number01+number02);
+
+switch(op){
+    case "+": lblOutput.innerHTML=number01+number02;break;
+
+}
+
+console.log(typeof number01);
+console.log(typeof number01);
+
+}
